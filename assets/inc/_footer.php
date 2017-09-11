bd6312655f61caeb0ceb522f9f36c34f1ad752f4
Marcel Haazen <mhaazen@live.nl>
2017-09-11 14:04:34 +0200

    Uploaded changes
    Removed unneeded code

--- assets/inc/_footer.php
+++ assets/inc/_footer.php
@@ -6,7 +6,7 @@
  * Time: 9:51 AM
  */
 ?>
-<footer class="footer bottom">
+<footer class="footer">
     <section class="container text-center">
         <p>Created by <a href="https://github.com/inpothet">Marcel Haazen</a> . Licensed under the<a href="https://github.com/milligram/milligram#license" title="MIT License" target="_blank"> MIT License</a>.
         </p>
